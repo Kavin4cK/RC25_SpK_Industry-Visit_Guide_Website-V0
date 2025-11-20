bb8549ddaeee01ababfa994b8f7e6ed6e4e27483
Kavin Krishnan. C <kavinkrishnanc@gmail.com>
2025-11-20 19:54:00 +0530

    BG Footer black for spk Logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
   const { theme } = useTheme()
 
   return (
-    <footer className="bg-white dark:bg-secondary-900 border-t border-secondary-200 dark:border-secondary-700">
+    <footer className="bg-white dark:bg-black border-t border-secondary-200 dark:border-secondary-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:py-8">
         <div className="flex flex-col md:flex-row items-center justify-between">
           {/* SPARK Logo */}
